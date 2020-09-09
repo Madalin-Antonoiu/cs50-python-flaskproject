30ba2d7b84cc24dc7b038cb7343f953a2076e5e1
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-09 14:39:44 +0300

    Now with Dynamic content and conditions inside template

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name="Madalin")
+    onetoten = random.randint(0, 10)
+    rnr = random.randint(0, 1)
+    return render_template("index.html", name="Madalin", number = rnr, onetoten=onetoten)
 
 @app.route("/about")
 def about():
