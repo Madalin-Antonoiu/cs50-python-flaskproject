90df8e5db3a15e019116cd0b2ffff895ebdf320b
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-14 16:42:41 +0300

    Updated the table and getting ready for the first registered user!

--- finance/app.py
+++ finance/app.py
@@ -19,14 +19,37 @@ db = SQL("sqlite:///finance.db")
 @app.route("/")
 # @login_required
 def index():
-    """Show portfolio of stocks"""
     # return apology("TODO")
-    return redirect("/login")
+    """Show portfolio of stocks"""
+    return render_template("/index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return "This is the register page"
+    if request.method == "GET":
+        return render_template( "register.html")
+    else:
+        username = request.form.get("username")
+        if not username:
+            return render_template("apology.html", message="You must provide an username.")
+
+        email = request.form.get("email")
+        if not email:
+            return render_template("apology.html", message="You must provide an email adress.")
+
+        password = request.form.get("password")
+        if not password:
+            return render_template("apology.html", message="You must provide a password.")
+
+        verify_password = request.form.get("verify_password")
+        if not verify_password:
+            return render_template( "apology.html", message="You must validate the password.")
+
+
+        # Only if you write a name and an email ( well, add a letter in each field) you can reach up to here where you write to the DB
+        db.execute("INSERT INTO users (email, username, password, verify_password) VALUES (:email, :username, :password, :verify_password);", email=email, username=username, password=password, verify_password=verify_password )
+        return redirect("/")
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    rows = db.execute("SELECT * from users;")
+    return render_template("login.html", rows=rows)
