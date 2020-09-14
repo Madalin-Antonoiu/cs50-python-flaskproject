79e194d52e2cec45ca4207aa17c6d67a3581532b
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-14 12:57:12 +0300

    Now final form. Will tackle CS50 Finance app next.

--- register/app.py
+++ register/app.py
@@ -15,6 +15,13 @@ def register():
         return render_template( "register.html")
     else:
         name = request.form.get("name")
+        if not name:
+              return render_template( "apology.html", message="You must provide a name.")
+
         email = request.form.get("email")
+        if not email:
+            return render_template( "apology.html", message="You must provide an email adress.")
+
+        # Only if you write a name and an email ( well, add a letter in each field) you can reach up to here where you write to the DB
         db.execute("INSERT INTO registrants (name, email) VALUES (:name, :email);", name=name, email=email)
         return redirect("/")
