5a440e4443ead20729055b2894edff521b64062b
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-15 08:04:31 +0300

    Now with Apology messages

--- finance/app.py
+++ finance/app.py
@@ -3,7 +3,8 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
-from tempfile import gettempdir
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import *
 
@@ -13,43 +14,98 @@ app = Flask(__name__)
 # The Model - Connect to database 
 db = SQL("sqlite:///finance.db")
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # if not os.environ.get("API_KEY"):
 #     raise RuntimeError("API_KEY not set.")
 
 @app.route("/")
 # @login_required
 def index():
-    # return apology("TODO")
+    return apology("TODO", 202)
     """Show portfolio of stocks"""
-    return render_template("/index.html")
+    # return render_template("/index.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "GET":
-        return render_template( "register.html")
+    if request.method == 'POST':
+        """Register user"""
+        username = request.form.get('username')
+        password = generate_password_hash(request.form.get('password'))
+        status = True
+        
+        # check username
+        if not request.form.get('username'):
+            status = False
+            return apology("You must provide an username")
+
+        # check password confirmation
+        if not request.form.get('password') == request.form.get('confirm_password'):
+            status = False
+            return apology("Password confirmation not match")
+
+        # check unique username
+        exists_username= db.execute("SELECT username FROM users where username = :username", username = username)
+        if exists_username:
+            status = False
+            return apology("Username already taken by another user")
+
+        # If the program didn't get into any of the above for the status to get false, it means we' re all good
+        if status:
+            # register
+            register = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)", username = username, hash = password)
+
+            # Remember which user has logged in
+            session["user_id"] = register
+
+            # Redirect user to home page
+            return redirect("/")
+
+        
+        # return render_template('register.html', status = status, text = text)
     else:
-        username = request.form.get("username")
-        if not username:
-            return render_template("apology.html", message="You must provide an username.")
+        return render_template('register.html')
 
-        email = request.form.get("email")
-        if not email:
-            return render_template("apology.html", message="You must provide an email adress.")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
 
-        password = request.form.get("password")
-        if not password:
-            return render_template("apology.html", message="You must provide a password.")
+    # Forget any user_id
+    session.clear()
+    rows = db.execute("SELECT * from users;")
 
-        verify_password = request.form.get("verify_password")
-        if not verify_password:
-            return render_template( "apology.html", message="You must validate the password.")
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
 
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
 
-        # Only if you write a name and an email ( well, add a letter in each field) you can reach up to here where you write to the DB
-        db.execute("INSERT INTO users (email, username, password, verify_password) VALUES (:email, :username, :password, :verify_password);", email=email, username=username, password=password, verify_password=verify_password )
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Ensure email was submitted
+        elif not request.form.get("email"):
+            return apology("must provide email", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
         return redirect("/")
 
-@app.route("/login")
-def login():
-    rows = db.execute("SELECT * from users;")
-    return render_template("login.html", rows=rows)
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html", rows=rows)
--- finance/helpers.py
+++ finance/helpers.py
@@ -1,68 +1,118 @@
-import csv
-import urllib.request
+# import requests
+# import urllib.parse
+# import datetime
 
-from flask import redirect, render_template, request, session, url_for
+from flask import redirect, render_template, request, session
 from functools import wraps
+from cs50 import SQL
 
-def apology(top="", bottom=""):
-    """Renders message as an apology to user."""
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
     def escape(s):
         """
         Escape special characters.
         https://github.com/jacebrowning/memegen#special-characters
         """
         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-            ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
             s = s.replace(old, new)
         return s
-    return render_template("apology.html", top=escape(top), bottom=escape(bottom))
+    return render_template("apology.html", message=escape(message), code=code)
+
 
 def login_required(f):
     """
     Decorate routes to require login.
-    http://flask.pocoo.org/docs/0.11/patterns/viewdecorators/
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            return redirect(url_for("login", next=request.url))
+            return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
 
-def lookup(symbol):
-    """Look up quote for symbol."""
 
-    # reject symbol if it starts with caret
-    if symbol.startswith("^"):
-        return None
+# def lookup(symbol):
+#     """Look up quote for symbol."""
 
-    # reject symbol if it contains comma
-    if "," in symbol:
-        return None
+#     # Contact API
+#     try:
+#         response = requests.get(f"https://api.iextrading.com/1.0/stock/{urllib.parse.quote_plus(symbol)}/quote")
+#         response.raise_for_status()
+#     except requests.RequestException:
+#         return None
 
-    # query Yahoo for quote
-    # http://stackoverflow.com/a/21351911
-    try:
-        url = "http://download.finance.yahoo.com/d/quotes.csv?f=snl1&s={}".format(symbol)
-        webpage = urllib.request.urlopen(url)
-        datareader = csv.reader(webpage.read().decode("utf-8").splitlines())
-        row = next(datareader)
-    except:
-        return None
+#     # Parse response
+#     try:
+#         quote = response.json()
+#         return {
+#             "name": quote["companyName"],#"JET",
+#             "price": float(quote["latestPrice"]),#310.01,
+#             "symbol": quote["symbol"] #"SSS4"
+#         }
+#     except (KeyError, TypeError, ValueError):
+#         return None
 
-    # ensure stock exists
-    try:
-        price = float(row[2])
-    except:
-        return None
 
-    # return stock's name (as a str), price (as a float), and (uppercased) symbol (as a str)
-    return {
-        "name": row[1],
-        "price": price,
-        "symbol": row[0].upper()
-    }
+# def usd(value):
+#     """Format value as USD."""
+#     return f"${value:,.2f}"
 
-def usd(value):
-    """Formats value as USD."""
-    return "${:,.2f}".format(value)
+# def getStocks():
+#     user_id = session['user_id']
+#     stocks = db.execute("SELECT * FROM users_stocks WHERE user_id = :user_id GROUP BY stock", user_id = user_id)
+#     data = {
+#         'balance': db.execute("SELECT cash FROM users where id = :user_id", user_id = user_id)[0]['cash'],
+#         'owned': stocksCurPrice(stocks),
+#     }
+#     data['stockBalance'] = TotalBalance(data['owned'])
+#     stocks = db.execute("SELECT * FROM transactions where user_id = :user_id ORDER BY date DESC", user_id = user_id)
+#     data['stocks'] = stocksCurPrice(stocks)
+#     return data
+
+# def TotalBalance(stocks):
+#     total = 0
+#     for stock in stocks:
+#         total = total + (stock['curPrice'] * stock['shares'])
+#     return total
+
+# def modStock(symbol, shares):
+#     user_id = session['user_id']
+#     # check if stock exists and update users stock shares
+#     if ownStock(symbol):
+#         db.execute("UPDATE users_stocks set shares = shares + :shares WHERE stock = :symbol AND user_id = :user_id",
+#         user_id = user_id, symbol = symbol, shares = shares)
+#     # else save new stock with shares
+#     else:
+#         db.execute("INSERT INTO users_stocks (user_id, stock, shares) VALUES(:user_id, :stock, :shares)",
+#         user_id = user_id, stock = symbol, shares = shares)
+
+# def ownStock(symbol):
+#     user_id = session['user_id']
+#     return db.execute("SELECT * FROM users_stocks WHERE user_id = :user_id AND stock = :symbol AND shares > 0",
+#     user_id = user_id, symbol = symbol)
+
+# def storeTxn(amount, type, symbol = None, shares = None):
+#     user_id = session['user_id']
+#     date = datetime.datetime.now()
+#     db.execute("INSERT INTO transactions (user_id, stock, price, shares, type, date) VALUES (:user_id, :symbol, :amount, :shares, :type, :date)"
+#     , amount = amount, symbol = symbol, date = date, user_id = user_id, shares = shares, type = type)
+
+# def updateCash(amount):
+#     user_id = session['user_id']
+#     db.execute("UPDATE users SET cash = cash + :newBalance where id = :user_id", user_id = user_id, newBalance = amount)
+
+# def stocksCurPrice(stocks):
+#     for stock in stocks:
+#         if stock['stock']:
+#             stock['curPrice'] = lookup(stock['stock'])['price']
+#         else:
+#             stock['curPrice'] = 0
+#     return stocks
+
+# def validate(symbol, shares):
+    # return (shares and symbol) or shares < 1
