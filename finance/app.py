240d120ff529c0493d6fcad840b9e581a0268ab7
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-16 10:16:50 +0300

    Now querying IEX API for results.

--- finance/app.py
+++ finance/app.py
@@ -1,11 +1,11 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for, request
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import login_required, apology
+from helpers import login_required, apology, lookup, usd
 
 # configure application
 app = Flask(__name__)
@@ -18,6 +18,20 @@ Session(app)
 
 # The Model - Connect to database 
 db = SQL("sqlite:///finance.db")
+
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    # os.environ["API_KEY"] = "KEY"
+    raise RuntimeError("API_KEY not set")
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 @app.route("/")
@@ -123,3 +137,27 @@ def logout():
     session.clear()
     flash("Successfully logged out.", "success")
     return redirect("/")
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Lookup for symbol"""
+    if request.method == "POST":
+        if not request.form.get("symbol"):
+            return apology("Must provide a symbol e.g TSLA" , 403, "quote.html") 
+        
+        symbol = request.form.get('symbol')
+        quote = lookup(symbol)
+
+        if not quote:
+            return apology("Not found.", 403, "quote.html")
+
+        return render_template('quote.html', quote = quote)
+
+        # return apology("Work in Progress" , 201, "quote.html") 
+    else:
+        # """Get stock quote."""
+        return render_template("quote.html")
+        
+
+
--- finance/helpers.py
+++ finance/helpers.py
@@ -1,6 +1,7 @@
-# import requests
-# import urllib.parse
-# import datetime
+import requests
+import os
+import urllib.parse
+import datetime
 
 from flask import redirect, render_template, request, session
 from functools import wraps
@@ -35,27 +36,35 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+def lookup(symbol):
+    """Look up quote for symbol."""
+    # https://cloud-sse.iexapis.com/stable/stock/TSLA/quote?token={API_KEY} - Works
+    # export API_KEY="value"
+    # Contact API
+    try:
+        api_key = os.environ.get("API_KEY")
+        # api_key = "pk_f80e2d59c5704109bbaade93cdc9ecce"
+        response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
+        print(response)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
 
-# def lookup(symbol):
-#     """Look up quote for symbol."""
+    # Parse response
+    try:
+        quote = response.json()
+        return {
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"]
+        }
+    except (KeyError, TypeError, ValueError) as error:
+        return (str(error))
 
-#     # Contact API
-#     try:
-#         response = requests.get(f"https://api.iextrading.com/1.0/stock/{urllib.parse.quote_plus(symbol)}/quote")
-#         response.raise_for_status()
-#     except requests.RequestException:
-#         return None
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
 
-#     # Parse response
-#     try:
-#         quote = response.json()
-#         return {
-#             "name": quote["companyName"],#"JET",
-#             "price": float(quote["latestPrice"]),#310.01,
-#             "symbol": quote["symbol"] #"SSS4"
-#         }
-#     except (KeyError, TypeError, ValueError):
-#         return None
 
 
 # def usd(value):
