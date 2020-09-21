a1ff5643af3d6d3904be93524c33da04e6378d05
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-21 13:50:18 +0300

    Now with password update password strenght check!

--- finance/app.py
+++ finance/app.py
@@ -417,7 +417,6 @@ def update_password():
         if password_test:
             return apology("Your new password does not meet the cryteria:  Length:8, Uppercase:1, Numbers:1, Special:1 ")
 
-
         if not request.form.get("current_password"):
             return apology("You have not typed in your current password") 
         
@@ -440,5 +439,6 @@ def update_password():
 
         flash("Successfully changed your password.", "success")
         return redirect("/")
+        
     else:
         return render_template("update-password.html")
